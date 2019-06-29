0f220409fb8a4c7ba5fa4ee6c8e3821c172ba92b
Vladimir Mikhalenko <mihalenko.w.a@gmail.com>
2019-06-29 17:45:55 +0300

    Fix injection ListDialogData, improve package.json scripts

--- packages/client/src/app/shared/list-dialog/list-dialog.component.ts
+++ packages/client/src/app/shared/list-dialog/list-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
-export interface ListDialogData {
+export class ListDialogData {
   title: string;
   names: string[];
   selected: number;
