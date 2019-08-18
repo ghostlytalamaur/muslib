666a7f83f47420cb2a6ff5900989a1dea57f6e88
Vladimir Mikhalenko <mihalenko.w.a@gmail.com>
2019-08-18 21:09:05 +0300

    Animate library router-outlet

--- packages/client/src/app/artists-library/artists-library/artists-library.component.ts
+++ packages/client/src/app/artists-library/artists-library/artists-library.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistsService } from '../services/artists.service';
 import { AlbumsService } from '../services/albums.service';
+import { ImagesService } from '../services/images.service';
+import { routerAnimation } from '../../animations';
 
 @Component({
   selector: 'app-artists-library',
   templateUrl: './artists-library.component.html',
   styleUrls: ['./artists-library.component.scss'],
-  providers: [ArtistsService, AlbumsService]
+  providers: [ArtistsService, AlbumsService, ImagesService],
+  animations: [routerAnimation]
 })
 export class ArtistsLibraryComponent implements OnInit {
   constructor(
